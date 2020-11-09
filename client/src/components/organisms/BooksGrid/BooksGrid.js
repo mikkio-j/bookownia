f5e0aeeb2baaee0d5d96d8b45e04e3e35a14dd87
mikolaj <mikolajsve@gmail.com>
2020-11-09 17:36:45 +0100

    add category select modal

--- client/src/components/organisms/BooksGrid/BooksGrid.js
+++ client/src/components/organisms/BooksGrid/BooksGrid.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Heading from '../../atoms/Heading/Heading';
@@ -20,11 +20,18 @@ const StyledHeaderSection = styled.div`
 `;
 
 const StyledButton = styled(Button)`
+  position: relative;
   width: 120px;
   height: 40px;
   font-size: 16px;
   text-transform: lowercase;
   cursor: pointer;
+  z-index: 10;
+  transition: 0.3s;
+  &.active {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
   i {
     margin-left: 5px;
   }
@@ -75,43 +82,101 @@ const StyledPagination = styled.ul`
   }
 `;
 
-const BooksGrid = ({ title }) => (
-  <>
-    <StyledHeaderSection>
-      <Heading>{title}</Heading>
-      <StyledButton>
-        kategorie <i className="fas fa-angle-down" />
-      </StyledButton>
-    </StyledHeaderSection>
-    <StyledGrid>
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-      <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
-    </StyledGrid>
-    <StyledPagination>
-      <li className="active">1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-    </StyledPagination>
-  </>
-);
+const StyledCategoryWrapper = styled.div`
+  position: relative;
+`;
+
+const StyledCompleteBlock = styled.div`
+  display: flex;
+  visibility: hidden;
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  top: 25px;
+  font-weight: 400;
+  background-color: #ffffff;
+  width: 100%;
+  border: 1px solid #acb4bb;
+  border-top: none;
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+  z-index: 5;
+  flex-direction: column;
+  justify-content: flex-start;
+  flex-grow: 1;
+  padding: 10px 0;
+  transition: 0.2s;
+  p {
+    font-family: 'Ubuntu';
+    color: #413d51;
+    margin: 0;
+    padding: 15px 5px;
+    text-align: right;
+    border-top: 1px solid rgba(0, 0, 0, 0.03);
+    transition: 0.2s;
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.03);
+      cursor: pointer;
+    }
+  }
+  &.active {
+    visibility: visible;
+    opacity: 1;
+  }
+`;
+
+const BooksGrid = ({ title }) => {
+  const [categoryModal, setCategoryModal] = useState(false);
+  return (
+    <>
+      <StyledHeaderSection>
+        <Heading>{title}</Heading>
+        <StyledCategoryWrapper>
+          <StyledButton
+            onClick={() => setCategoryModal(!categoryModal)}
+            className={categoryModal && 'active'}
+          >
+            kategorie <i className="fas fa-angle-down" />
+          </StyledButton>
+          <StyledCompleteBlock className={categoryModal && 'active'}>
+            <p>Kryminały</p>
+            <p>Fantastyka</p>
+            <p>Reportaż</p>
+            <p>Dla dzieci</p>
+            <p>Kulinaria</p>
+          </StyledCompleteBlock>
+        </StyledCategoryWrapper>
+      </StyledHeaderSection>
+      <StyledGrid>
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+        <Book bookCover={BookCover} title="Lockdown" author="Robert Ziębiński" price={25} />
+      </StyledGrid>
+      <StyledPagination>
+        <li className="active">1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </StyledPagination>
+    </>
+  );
+};
 
 BooksGrid.propTypes = {
   title: PropTypes.string.isRequired,
